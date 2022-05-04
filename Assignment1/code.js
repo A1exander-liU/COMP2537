c3e856c4747fdb7bc400f4d8b95564bf85850dbc
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-03 17:16:21 -0700

    implement get_this_pokemon_info

--- Assignment1/code.js
+++ Assignment1/code.js
@@ -6,10 +6,20 @@ function change_type_background(type_name) {
     return `<span class='${type_name}'>${captialize(type_name)}</span>`
 }
 
+function get_this_pokemon_info() {
+    $.ajax(
+        {
+            "url": `https://pokeapi.co/api/v2/pokemon/${$(this).attr("id")}`,
+            "type": "GET",
+            "success": display_this_pokemon
+        }
+    )
+}
+
 function display_random_pokemons(data) {
     old = $(".pokemons").html()
     result = ""
-    result += "<div class='pokemon'>"
+    result += `<div class='pokemon' id='${data.name}'>`
     result += `<p>${captialize(data.name)}</p>`
     result += `<img src='${data.sprites.other["official-artwork"].front_default}'>`
     for (i = 0; i < data.types.length; i++) {
@@ -33,6 +43,7 @@ function get_random_pokemons() {
 
 function setup() {
     get_random_pokemons()
+    $('.pokemon').click(get_this_pokemon_info)
 }
 
 $(document).ready(setup)
