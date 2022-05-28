b3009ee0f42fa7cac0e5a808f1140b6da9e5a2e4
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-27 21:03:10 -0700

    implement code to get random pokemons

--- Assignment4/public/memory-game.js
+++ Assignment4/public/memory-game.js
@@ -1,5 +1,26 @@
+poke_cards = []
+poke_cards_copy = []
+
+function get_card_amount() {
+    if ($("#card-amount").val() > 0) {
+        console.log($("#card-amount").val())
+        poke_cards = []
+        poke_cards_copy = []
+        for (i = 0; i < $("#card-amount").val(); i++) {
+            console.log(Math.floor(1 + (Math.random() * 897)))
+            $.ajax(
+                {
+                    "url": `https://pokeapi.co/api/v2/pokemon/${Math.floor((Math.random() * 897) + 1)}`,
+                    "type": "GET",
+                    "success": build_memory_cards
+                }
+            )
+        }
+    }
+}
+
 function setup() {
-    
+    $("button").click(get_card_amount)
 }
 
 $(document).ready(setup)
