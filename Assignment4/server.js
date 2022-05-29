b3de5e29f8dc45aafc1f54430dc9fd65a6197446
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-28 22:35:24 -0700

    change name to err and data

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -494,7 +494,7 @@ app.post("/getAdmin", function(req, res) {
 })
 
 app.delete("/deleteUser", function(req, res) {
-    userModel.deleteOne({username: req.body.username}, function(req, res) {
+    userModel.deleteOne({username: req.body.username}, function(err, data) {
         if (err) {
             console.log("Err" + err)
         }
