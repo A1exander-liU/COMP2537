0ec4c2f0b6e9127fca03d401cefda10cb31f8ffb
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-28 18:48:28 -0700

    change bg colour based on current tab

--- Assignment4/public/admin_dashboard.js
+++ Assignment4/public/admin_dashboard.js
@@ -1,8 +1,19 @@
+current_tab = ""
+
+function change_page() {
+    current_tab = $(this).attr("id")
+    $(".dashboard-tab").removeClass("active")
+    $(`#${current_tab}`).addClass("active")
+}
+
 function return_to_home() {
     location.href = "/pokedex"
 }
 
 function setup() {
+    $(".dashboard-tab").removeClass("active")
+    $("#users").addClass("active")
+    $(".dashboard-tab").click(change_page)
     $(".go-back-home").click(return_to_home)
 }
 
