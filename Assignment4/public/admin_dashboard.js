63c874d68f7aab2eef28f2ddb12d9318d02a597f
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-28 20:18:22 -0700

    load user info on users table

--- Assignment4/public/admin_dashboard.js
+++ Assignment4/public/admin_dashboard.js
@@ -1,5 +1,38 @@
 current_tab = ""
 
+{/* <div class="users-container-item">
+<p>4</p>
+<p>4</p>
+<p>User <i class="fa-solid fa-pencil"></i></p>
+</div> */}
+
+function display_user_data(data) {
+    $(".users-container").html("")
+    console.log(data)
+    for (i = 0; i < data.length; i++) {
+        user = ""
+        user += `<div class="users-container-item" id="${data[i].username}">`
+
+        user += `<p>${data[i].username}</p>`
+        user += `<p>${data[i].password}</p>`
+        user += `<p>${data[i].type} <i class="fa-solid fa-pencil"></i></p>`
+
+        user += `</div>`
+        old = $(".users-container").html()
+        $(".users-container").html(old + user)
+    }
+}
+
+function load_user_data() {
+    $.ajax(
+        {
+            "url": "/getUsers",
+            "type": "GET",
+            "success": display_user_data
+        }
+    )
+}
+
 function change_page() {
     current_tab = $(this).attr("id")
     $(".dashboard-tab").removeClass("active")
@@ -13,12 +46,14 @@ function return_to_home() {
 }
 
 function setup() {
+    load_user_data()
     $(".dashboard-tab").removeClass("active")
     $("#users").addClass("active")
     $(".dashboard-page").hide()
     $("#users-page").show()
     $(".dashboard-tab").click(change_page)
     $(".go-back-home").click(return_to_home)
+    $("#users").click(load_user_data)
 }
 
 $(document).ready(setup)
